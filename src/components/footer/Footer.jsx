47f1049ae19ac5f4dba3352fdfe3b5d3d499dc00
MD REHAN <mdray283@gmail.com>
2023-11-23 20:14:12 +0530

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Find Millions of Movies Tv Shows on Our App . 
+          This App is made for those who have keen interest in Movies Show And Tv Shows.
+          Keep Enjoying those shows and Movies and keep Supporting Us ,
+          Your Support is Motivation for us. Thanks ❤️❤️❤️
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+            <a href=""></a>
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
